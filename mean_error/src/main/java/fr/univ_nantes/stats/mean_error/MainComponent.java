c3b17ae6c4e0942e89dc33fa286b310951bd3e95
Gpotier <guillaume.potier@univ-nantes.fr>
2019-09-04 11:06:27 +0200

    remove mean_error

--- mean_error/src/main/java/fr/univ_nantes/stats/mean_error/MainComponent.java
+++
@@ -1,8 +0,0 @@
-package fr.univ_nantes.stats.mean_error;
-
-import dagger.Component;
-
-@Component
-public interface MainComponent {
-    void inject(Main main);
-}
--- tre_half_normal/src/cluster/stat-mean_error.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar stat-mean_error.jar > stat-mean_error.out
